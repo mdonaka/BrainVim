1fea879d7565862eedf6eaf45d67163f6d8e9cc1
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2022-12-13 02:19:17 +0900

    add macros

--- convert.sh
+++ convert.sh
@@ -21,12 +21,16 @@ read (){
 FILE=$1
 
 MACRO_PRE=`read brain_command/macros/preprocess`
+MACRO_E=`read brain_command/macros/e`
 MACRO_A=`read brain_command/macros/a`
 MACRO_X=`read brain_command/macros/x`
 MACRO_S=`read brain_command/macros/s`
 MACRO_L=`read brain_command/macros/l`
 MACRO_R=`read brain_command/macros/r`
 MACRO_D=`read brain_command/macros/d`
+MACRO_F=`read brain_command/macros/f`
+MACRO_G=`read brain_command/macros/g`
+MACRO_H=`read brain_command/macros/h`
 
 PREPROCESS=`read brain_command/preprocess`
 POSTPROCESS=`read brain_command/postprocess`
@@ -48,7 +52,7 @@ DATA=${DATA//>/$RIGHT_ANGLE}
 DATA=${DATA//[/$LEFT_SQUARE}
 DATA=${DATA//]/$RIGHT_SQUARE}
 
-echo $MACRO_PRE$MACRO_A$MACRO_X$MACRO_S$MACRO_L$MACRO_R$MACRO_D > command.vim
+echo $MACRO_PRE$MACRO_A$MACRO_E$MACRO_X$MACRO_S$MACRO_L$MACRO_R$MACRO_D$MACRO_F$MACRO_G$MACRO_H > command.vim
 echo $PREPROCESS >> command.vim
 echo $DATA >> command.vim
 echo $POSTPROCESS >> command.vim
